25840bbffe42c46a0e10c34254cb5ed466b9bb71
Pranav Gupta <partholikesit@gmail.com>
2012-09-26 00:16:41 +0530

    modified server side file getData.php

--- getData.php
+++ getData.php
@@ -1,4 +1,12 @@
 <?php
-	$docText = $_POST['docText'];
-	echo "Data received successfully";
+	if(isset($_POST['docText']))
+		$docText = $_POST['docText'];
+	else
+		$docText = $_GET['docText'];
+	$con = mysql_connect("localhost", "root", "1jmpgaafb");
+	mysql_select_db("webBook");
+	$query = "insert into annotated_pages values('" . $_SERVER['REMOTE_ADDR'] . "', '$docText')";
+	mysql_query($query);
+	mysql_close($con);
+	echo $query;
 ?>
