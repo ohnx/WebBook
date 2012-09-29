d524b1ea8a94e080340aef327d5f1cacef698b42
Pranav Gupta <partholikesit@gmail.com>
2012-09-29 23:22:46 +0530

    updated content.js and manifest

--- background.js
+++ background.js
@@ -1,20 +1,53 @@
-chrome.tabs.onSelectionChanged.addListener(function(tab) {
+var oauth = ChromeExOAuth.initBackgroundPage({
+  'request_url': 'https://www.google.com/accounts/OAuthGetRequestToken',
+  'authorize_url': 'https://www.google.com/accounts/OAuthAuthorizeToken',
+  'access_url': 'https://www.google.com/accounts/OAuthGetAccessToken',
+  'consumer_key': 'anonymous',
+  'consumer_secret': 'anonymous',
+  'scope': 'http://www.google.com/m8/feeds/',
+  'app_name': 'WebBook'
+});
+
+function gAuthorize() {
+	alert('browserAction Clicked');
+//	if(oauth.hasToken()){
+//		//alert('token already present');
+//		chrome.tabs.executeScript(null, {file:"sendData.js", allFrames:true});		
+//	}
+//	else{
+		oauth.authorize(onAuthorize);
+//	}
+}
+
+function onAuthorize(){
+	//alert('user authorized and access granted');
+	//console.log("user authorized and access granted");
+	//chrome.tabs.executeScript(null, {file:"sendData.js", allFrames:true});
+}
+
+function handleRequest(request, sender, sendResponse){
+	var docText = request.data;
+	alert(docText);
+
+	var url = "http://172.16.25.171/webBook/getData.php";
+	var xmlhttp = new XMLHttpRequest();
+	xmlhttp.open("post", url, true);
+	xmlhttp.onload = function(){alert('data successfully transferred');};
+	xmlhttp.send('docText=' + docText);
+
+}
+
+function activateHighlighter(tab) {
 //	chrome.tabs.executeScript(null, {code:"alert(window.getSelection().toString());"});
 //	chrome.tabs.executeScript(null, {code:"document.body.setAttribute('onmouseup','alert(window.getSelection().toString())');"});
 //	chrome.tabs.executeScript(null, {code:"document.body.bgColor = 'red';"});
 	chrome.tabs.executeScript(null, {file:"content.js", allFrames:true});
-});
+}
 
-/*chrome.tabs.onUpdated.addListener(function(tab) {
-	chrome.tabs.executeScript(null, {file:"content.js", allFrames:true});
-});
-*/
+chrome.tabs.onSelectionChanged.addListener(activateHighlighter);
+chrome.browserAction.onClicked.addListener(gAuthorize);
 
-chrome.browserAction.onClicked.addListener(function(tab) {
-	chrome.tabs.executeScript(null, {file:"sendData.js", allFrames:true});
-});
+//chrome.tabs.onUpdated.addListener(activateHighlighter);
+chrome.extension.onRequest.addListener(handleRequest);
 
-chrome.extension.onRequest.addListener(function(request, sender, sendResponse){
-	chrome.tabs.executeScript(null, {code:"alert(request.data);"});	
-});
 
--- content.js
+++ content.js
@@ -1,52 +1,5 @@
-/*document.addEventListener('mouseup', function(e){
-//	chrome.extension.sendRequest({"data":window.getSelection().toString()}, function(response){});
-	var selection = window.getSelection();
-	if(selection.toString().length == 0){
-		return;
-	}
-	
-	var range;
-
-	if (selection.getRangeAt)
-	{
-		alert(selection.toString());
-		range = selection.getRangeAt(0);
-	}
-	else
-	{
-		alert("else");
-		// Older WebKit doesn't have getRangeAt
-		range.setStart(selection.anchorNode, selection.anchorOffset);
-		range.setEnd(selection.focusNode, selection.focusOffset);
-
-		// Handle the case when the selection was selected backwards (from the end to the start in the
-		// document)
-		if (range.collapsed !== selection.isCollapsed)
-		{
-			range.setStart(selection.focusNode, selection.focusOffset);
-			range.setEnd(selection.anchorNode, selection.anchorOffset);
-		}
-	}
-	//alert(range);
-	//range.collapse(false);
-
-	// Create the marker element containing a single invisible character using DOM methods and insert it
-	var markerEl = document.createElement("span");
-	var selectionContents = range.extractContents();
-	markerEl.id = "webBookHighlight";
-	markerEl.style.background = "red";
-	markerEl.appendChild(selectionContents);
-	range.insertNode(markerEl);
-});
-*/
-
-document.addEventListener('keypress', function(e){
-	var unicode=e.charCode? e.charCode : e.keyCode
-	var actualkey=String.fromCharCode(unicode);
-	
-	if(actualkey != "h"){
-		return;
-	}
+//alert('content.js running for WebBook');
+function highlightSelection(){
 	var selection = window.getSelection();
 	if(selection.toString().length == 0){
 		return;
@@ -61,4 +14,24 @@ document.addEventListener('keypress', function(e){
 	markerEl.style.background = "red";
 	markerEl.appendChild(selectionContents);
 	range.insertNode(markerEl);
-});
+}
+
+function sendData(){
+	var docText = document.body.innerHTML;
+	chrome.extension.sendRequest({"data":docText});
+}
+
+function performAction(e){
+	var unicode=e.charCode? e.charCode : e.keyCode
+	var actualkey=String.fromCharCode(unicode);
+	
+	if (actualkey == 'h'){
+		highlightSelection();
+	}
+	else if (actualkey == 's'){
+		sendData();
+	}
+}
+
+document.addEventListener('keypress', performAction);
+
